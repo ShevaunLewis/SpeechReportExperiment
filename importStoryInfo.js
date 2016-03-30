1163e332a767d447e273e4462b0f51fa1d9ccd73
ShevaunLewis <shevaun.lewis@gmail.com>
2016-03-30 16:59:36 -0400

    Add CSV files with story info, Simplify display updates

---
+++ importStoryInfo.js
@@ -0,0 +1,54 @@
+var imageFiles = {};
+var stories;
+
+// create an associative array of image filenames
+// has the form {Dora: "img/characters/Dora.png", Swiper: "...", ...}
+Papa.parse("imageFiles.csv", {
+    download: true,
+    header: true,
+    complete: function(results) {
+	var imageArray = results.data;
+	imageArray.forEach(function (it) {
+	    imageFiles[it.name] = it.imgFile;
+	});
+    }
+});
+
+// create an array of story objects
+Papa.parse("storyInfo.csv", {
+    download: true,
+    header: true,
+    complete: function(results) {
+	stories = results.data;
+    }
+});
+
+// create a nested associative array of text for the stories
+// first layer of keys is the storyIds ("1","2","3",...)
+// second layer of keys is the story phases ("hi","scene1",...)
+// each phase is associated with an ARRAY of sentences
+// each sentence object includes properties like cond, order, text, etc.
+// the information from the text hash then gets added to the storyArray,
+// so that each story has a property "text" containing the associative array
+// of the phases.
+Papa.parse("storyText.csv", {
+    download: true,
+    header: true,
+    complete: function(results) {
+	var text = results.data;
+	var textHash = {};
+	text.forEach(function (it) {
+	    if (!(it.storyId in textHash)) {
+		textHash[it.storyId] = {};
+	    }
+	    if (it.phase in textHash[it.storyId]) {
+		textHash[it.storyId][it.phase].push(it);
+	    } else {
+		textHash[it.storyId][it.phase] = [it];
+	    }
+	});
+	stories.forEach(function (it) {
+	    it.text = textHash[it.storyId];
+	});
+    }
+});
--- main.js
+++ main.js
@@ -1,72 +1,162 @@
-//samples
-
-var path = "img/backgrounds/path.png";
-var dora = "img/characters/Dora.png";
-var swiper = "img/characters/Swiper.png";
-var boots = "img/characters/Boots.png";
-var basket = "img/objects/basket.png";
-var blanket = "img/objects/blanket.png";
-var sandwich = "img/objects/sandwich.png";
-var story1 = new Story(dora, swiper, boots, path, path, basket, blanket, sandwich)
-
-
-function updateText() {
-    $("#status").html("SubjID: " + subjID + "<br/>");
-}
+var currentStory = 0;
+var currentPhase = 0;
+var phases = ["intro","scene1","give1","give2","scene2","take1","take2","end"];
+var subjInfo = {};
 
 // Collect subject information from form on start page
 $("#submitForm").click(saveSubjInfo);
 
-var subjID = "not set yet";
-var date, script;
-
 function saveSubjInfo() {
-    subjID = $("#subjID").val();
-    date = $("#date").val();
-    script = $("#script").val();
-    updateText();
+    //Collect information from form
+    subjInfo.subjId = $("#subjID").val();
+    subjInfo.date = $("#date").val();
+    subjInfo.script = $("#script").val();
+    //Update subject info at bottom of page
+    $("#subjInfo").html("Subj: " + subjInfo.subjId + "   Date: " + subjInfo.date + "   Script: " + subjInfo.script);
     $("#subjForm").remove(); // get rid of form
-    showIntro(story1);
+    updateDisplay();
 }
 
-
-
-
-function showScene1(story) {
-    $(".backgroundImg").attr("src",story.mainBg);
-    $("#narrImg").attr("src",story.narr);
-    $("#c1Img").attr("src",story.c1);
-    $("#c2Img").attr("src",story.c2);
-    $("#nextButton").click(function(){
-	showGiveTrial(story,1);
-    });
+function updateStatus() {
+    $("#expStatus").html("Story: " + stories[currentStory].storyId + "   Phase: " + phases[currentPhase]);
 }
 
-function showIntro(story) {
-    $(".backgroundImg").attr("src",story.introBg);
-    $("#narrImg").attr("src",story.narr);
-    $("#nextButton").click(function(){
-        showScene1(story);
-    });
-}
+$("#nextButton").click(function(){
+    updateDisplay();
+});
 
-function showGiveTrial(story,trialnum) {
-    if (trialnum == 1) {
-	$("#objImg").attr("src",story.giveObj1);
-	$("#nextButton").click(function(){
-	    showGiveTrial(story,2);
-	});
-    } else if (trialnum == 2) {
-	$("#objImg").attr("src",story.giveObj2);
-	$("#nextButton").click(function(){
-	    showScene2(story);
-	});
+function updateDisplay(){
+    updateStatus();
+    var story = stories[currentStory];
+    console.log("Story: ", currentStory, "Phase: ", currentPhase);
+    switch(currentPhase) {
+    case 0: //intro
+	if (currentStory > 0) {
+	    resetDisplay();
+	}
+	$(".backgroundImg").attr("src",imageFiles[story.bg1]);
+	$("#narrImg").attr("src",imageFiles[story.narrator]);
+	break;
+    case 1: //scene1
+	$("#narrImg").attr("src",imageFiles[story.narrator]);
+	$("#c1Img").attr("src",imageFiles[story.c1]);
+	$("#c2Img").attr("src",imageFiles[story.c2]);
+	break;
+    case 2: //give1
+	$("#objImg").attr("src",imageFiles[story.giveObj1]);
+	break;
+    case 3: //give2
+	$("#objImg").attr("src",imageFiles[story.giveObj2]);
+ 	break;
+    case 4: //scene2
+	$(".backgroundImg").attr("src",imageFiles[story.bg2]);
+	break;
+    case 7:
+	if (currentStory < 7) {
+	    currentStory++;
+	}
     }
+    if (currentPhase < 7) {
+	currentPhase++;
+    } else {
+	currentPhase = 0;
+    }
+    console.log("Story: ", currentStory, "Phase: ", currentPhase);
 }
 
-function showScene2(story) {
+function resetDisplay(){
+    var displayImages = ["#backgroundImg","#narrImg","#c1Img","#c2Img"]
+    displayImages.forEach(function(it) {
+	$(it).removeAttr("src");
+    });
+    var displayContainers = ["#narrTarget","#c1Target","#c2Target","#objStart"];
+    displayContainers.forEach(function(it) {
+	$(it).empty();
+    });
 }
 
+// function showIntro() {
+//     var story = stories[currentStory];
+//     $(".backgroundImg").attr("src",imageFiles[story.bg1]);
+//     $("#narrImg").attr("src",imageFiles[story.narrator]);
+//     $("#nextButton").click(function(){
+//         showScene1(story);
+//     });
+//     updateStatus();
+// }
+
+// function showScene1() {
+//     var story = stories[currentStory];
+//     currentPhase = "scene1";
+//     $("#narrImg").attr("src",imageFiles[story.narrator]);
+//     $("#c1Img").attr("src",imageFiles[story.c1]);
+//     $("#c2Img").attr("src",imageFiles[story.c2]);
+//     $("#nextButton").click(function(){
+// 	showGiveTrial(1);
+//     });
+//     updateStatus();
+// }
+
+// function showGiveTrial(trialnum) {
+//     var story = stories[currentStory];
+//     if (trialnum == 1) {
+// 	currentPhase = "give1";
+// 	$("#objImg").attr("src",imageFiles[story.giveObj1]);
+// 	$("#nextButton").click(function(){
+// 	    showGiveTrial(2);
+// 	});
+//     } else if (trialnum == 2) {
+// 	currentPhase = "give2";
+// 	$("#objImg").attr("src",story.giveObj2);
+// 	$("#nextButton").click(function(){
+// 	    showScene2();
+// 	});
+//     }
+//     updateStatus();
+// }
+
+// function showScene2() {
+//     var story = stories[currentStory];
+//     currentPhase = "scene2"
+//     $("#nextButton").click(function(){
+// 	showTakeTrial(1);
+//     });
+//     updateStatus();
+// }
+
+// function showTakeTrial(trialnum) {
+//     var story = stories[currentStory];
+//     if (trialnum == 1) {
+// 	currentPhase = "take1";
+// 	$("#nextButton").click(function() {
+// 	    showTakeTrial(2);
+// 	});
+//     } else if (trialnum == 2) {
+// 	currentPhase = "take2";
+// 	$("#nextButton").click(function() {
+// 	    showEnd();
+// 	});
+//     }
+//     updateStatus();
+// }
+
+// function showEnd() {
+//     var story = stories[currentStory];
+//     currentPhase = "end";
+//     updateStatus();
+//     $("#nextButton").click(function() {
+// 	if (currentStory < 8) {
+// 	    currentStory++;
+// 	    showIntro();
+// 	} else {
+// 	    endExperiment();
+// 	}
+//     });
+// }
+
+// function endExperiment() {
+    
+    
 // function showIntro(storyId) {
 //     $(".backgroundImg").attr("src",storyId.introBg);
 //     $("#narrImg").attr("src",storyId.narr);
